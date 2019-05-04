e868f568aabecd1477db612abb80543259db05dd
Caro Meysmans <caro.meysmans@gmail.com>
2019-05-04 11:12:54 +0200

    Update synchronize.py to use common framework

--- test/synchronize.py
+++ test/synchronize.py
@@ -1,50 +1,29 @@
+# Scientific computing
+import numpy
+
 # Time access and conversions
 import time
+import datetime
 
 # Control measurement devices
-import visa
-
-
-# Create resource manager
-rm = visa.ResourceManager()
-
-## FPGA
-# Connect to the FPGA
-fpga = rm.open_resource('ASRL5::INSTR')
-
-# Query identification string
-fpga_idn = fpga.query('*IDN?')
-
-# Check identification string
-if fpga_idn != 'IDLAB,AETHER,0.1,0.1\r\r\n':
-    print('Check the connection to the FPGA!')
-else:
-    print('Successfully connected to the FPGA.')
+import common
 
 ## Oscilloscope
-# Connect to the oscilloscope
-oscilloscope = rm.open_resource('GPIB1::8::INSTR')
+oscilloscope = common.Oscilloscope('GPIB1::8::INSTR')
+oscilloscope.system_header('OFF')
+oscilloscope.timebase_precision_radio_frequency('2E9')
+oscilloscope.timebase_precision('ON')
+oscilloscope.timebase_scale('100E-12')
+oscilloscope.channel_display(3, 'ON')
+oscilloscope.channel_scale(3, 100E-3)
+oscilloscope.channel_display(4, 'ON')
+oscilloscope.channel_scale(4, 100E-3)
+oscilloscope.measure_define_deltatime('RISING', 1, 'MIDDLE', 'RISING', 1, 'MIDDLE')
 
-# Query identification string
-oscilloscope_idn = oscilloscope.query('*IDN?')
-
-# Check identification string
-if oscilloscope_idn != 'Agilent Technologies,86100C,MY43490120,A.09.01\n':
-    print('Check the connection to the oscilloscope!')
-else:
-    print('Successfully connected to the oscilloscope.')
-
-# Define measurement
-oscilloscope.write(':MEASURE:DEFINE DELTATIME, RISING, 1, MIDDLE, RISING, 1, MIDDLE')
-oscilloscope.write(':SYSTEM:HEADER OFF')
-
-# Reset
-fpga.write('TRANSCEIVERS:RESET')
-
-time.sleep(2)
-
-# Select channel
-fpga.write('TRANSCEIVERS:SELECT OUTPUT5')
+## FPGA
+fpga = common.FPGA('ASRL5::INSTR')
+fpga.transceivers_reset()
+fpga.transceivers_select('OUTPUT5')
 
 # This is large in terms of us...
 deltatime = 1
@@ -52,12 +31,11 @@ deltatime = 1
 while abs(deltatime) > 1e-12:
     if deltatime > 0:
         # Decrement phase
-        fpga.write('TRANSCEIVERS:PHASE -1')
+        fpga.transceivers_phase(-1)
     else:
         # Increment phase
-        fpga.write('TRANSCEIVERS:PHASE 1')
+        fpga.transceivers_phase(1)
 
-    deltatime = float(oscilloscope.query(':MEASURE:DELTATIME? CHANNEL3,CHANNEL4'))
+    deltatime = oscilloscope.measure_deltatime('CHANNEL3', 'CHANNEL4')
 
     time.sleep(0.05)
-
