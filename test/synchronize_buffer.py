fb362d70b41132f711a644aa64bf97f0a67cf073
Caro Meysmans <caro.meysmans@gmail.com>
2019-05-08 12:01:28 +0200

    Add synchronization script

--- test/synchronize_buffer.py
+++ test/synchronize_buffer.py
@@ -5,8 +5,6 @@ import common
 fpga = common.FPGA('ASRL5::INSTR')
 fpga.transceivers_reset()
 
-fpga.transceivers_select('OUTPUT5')
-fpga.transceivers_synchronize()
-
-fpga.transceivers_select('OUTPUT6')
-fpga.transceivers_synchronize()
+for i in range(0, 9):
+    fpga.transceivers_select('OUTPUT{}'.format(i))
+    fpga.transceivers_synchronize()
